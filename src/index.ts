6532bfc7c3ebae5b0f8f3885e86b51e091d9f022
hamuyuuki <13702378+hamuyuuki@users.noreply.github.com>
2021-02-20 13:03:26 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import {getInputs, getContext} from './actions-toolkit'
+import {
+  currentPushableHours,
+  updateBranchRestrictionRule
+} from './merging-hours-restriction'
+
+async function run(): Promise<void> {
+  const inputs = getInputs()
+  const context = getContext()
+
+  currentPushableHours(inputs.startHour, inputs.endHour)
+}
+
+run()
